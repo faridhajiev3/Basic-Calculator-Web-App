a568a54ad18572c02febbffcfa422bc54aa28f27
FaridHajiyev3 <faridhajiev3@gmail.com>
2025-06-01 12:40:04 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".btn");
+const darkMode = document.querySelector(".dark-mode")
+const container = document.querySelector(".container")
+
+
+let currentInput = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.dataset.value;
+
+        if (value === "C") {
+            currentInput = "";
+        } else if (value === "=") {
+            currentInput = eval(currentInput);
+
+        }
+        else if (value === "back") {
+            currentInput = currentInput.slice(0, -1);
+        }
+        else {
+            currentInput += value;
+        }
+
+        display.value = currentInput;
+    });
+});
+
+//Klaviatura dəstəyi
+document.addEventListener("keydown", (e) => {
+    const key = e.key;
+
+
+    if ("0123456789+-*/.".includes(key)) {
+        currentInput += key;
+    } else if (key === "Enter") {
+        currentInput = eval(currentInput).toString();
+    } else if (key === "Backspace") {
+        currentInput = currentInput.slice(0, -1);
+    }
+
+    display.value = currentInput;
+});
+
+//Gece rejmi 
+darkMode.addEventListener("click", () => {
+    darkMode.classList.toggle("light-mode")
+    container.classList.toggle("dark-theme");
+})
